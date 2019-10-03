8e787872c252875046040cb3a6146416aaa8a413
Isabella Mota Franco <1802189@SALAS.AULAS>
2019-10-03 11:07:35 -0300

    Adicionando função subtrair

--- Principal.py
+++ Principal.py
@@ -3,5 +3,8 @@ def somar (x,y):
 
 def multiplicar (x,y):
     return x * y
+
+def subtrair (x,y):
+    return x - y
     
     #Comentario
--- Teste.py
+++ Teste.py
@@ -1,10 +1,13 @@
 import pytest
 from Principal import somar
 from principal import multiplicar
-
+from principal import subtrair
 
 def teste_somar():
     assert somar(2,4)==6
 
 def teste_multiplicar():
     assert multiplicar(6,12)==72
+
+def teste_subtrair():
+    assert subtrair(290-800)==610
